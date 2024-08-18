a990d9afc45b0cfe4b878817ba4c83a371e05562
Vincent Toledo <toledov@ufl.edu>
2024-08-18 13:22:43 -0400

    dd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flexContainer'> 
+      <h1 className = 'nameTitle'style = {{ fontWeight: 'bold'}}> Vincent A. Toledo</h1>
+      <div className='regTitle'>I'm a junior Computer Science student at the University of Florida</div>
+      <div style={{ margin: '100px' }}> </div>
+      <div className='regTitle'>What I've been working On...</div>
+      <p>sdfsadfsdfasdfasdfasdfasfd</p>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
